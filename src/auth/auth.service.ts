86642a16e4ba3530a19f734582b1abbff043eea3
ryannath <z5258025@ad.unsw.edu.au>
2024-03-10 23:39:52 +1100

    Using dto and added initial auth logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { omit } from 'src/helper/omit';
 
 @Injectable()
 export class AuthService {
-  signup() {
-    return 'I am signup';
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDto) {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+    // save the new user in the db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash: hash,
+      },
+    });
+    // return the saved user
+    const cleanedUser = omit(user, 'hash');
+    return cleanedUser;
   }
 
-  signin() {
-    return 'I am signin';
+  signin(dto: AuthDto) {
+    console.log(dto);
+    return { message: 'Successfully signed in' };
   }
 }
