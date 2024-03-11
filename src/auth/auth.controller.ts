696c1beaf032625ef36105dd1283993d05487c65
ryannath <ryan.nathan@hotmail.com>
2024-03-11 14:41:39 +1100

    Added e2e auth testing

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
@@ -12,6 +12,7 @@ export class AuthController {
   }
 
   @Post('signin')
+  @HttpCode(200)
   signin(@Body() dto: AuthDto) {
     return this.authService.signin(dto);
   }
